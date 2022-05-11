0914ef17dd81388a89bc89ddd34f550bcbcad0dc
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-11 21:18:15 +0300

    added shared module to user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { SharedModule } from 'src/app/shared/shared.module';
+
 // Component
 import { AuthModalComponent } from 'src/app/user/auth-modal/auth-modal.component';
 
 @NgModule({
   declarations: [AuthModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
