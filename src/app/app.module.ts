883410725b8bcfdf4545741bf12e7b24750faf61
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-11 20:53:47 +0300

    created auth-modal and nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// Components
-import { AppComponent } from 'src/app/app.component';
-
 // Modules
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { UserModule } from 'src/app/user/user.module';
 
+// Components
+import { AppComponent } from 'src/app/app.component';
+import { NavComponent } from './nav/nav.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Component
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [AuthModalComponent],
   imports: [CommonModule],
 })
 export class UserModule {}
