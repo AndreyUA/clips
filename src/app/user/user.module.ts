694f5748b8596e810813ce45c57dbf51ba54f23d
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-11 20:57:48 +0300

    added exports to UserModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Component
-import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { AuthModalComponent } from 'src/app/user/auth-modal/auth-modal.component';
 
 @NgModule({
   declarations: [AuthModalComponent],
   imports: [CommonModule],
+  exports: [AuthModalComponent],
 })
 export class UserModule {}
