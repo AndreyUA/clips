8260c4c4360f852ded092fa13a7770dbed7e235b
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-11 20:47:21 +0300

    created user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// Components
+import { AppComponent } from 'src/app/app.component';
+
+// Modules
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { UserModule } from 'src/app/user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
