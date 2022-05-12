d243f96d6103ecd37d1b918b4578b7ed3380ca3c
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-12 21:25:51 +0300

    first DI

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -5,4 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class ModalService {
   constructor() {}
+
+  visible: boolean = false;
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { ModalService } from 'src/app/services/modal.service';
+
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
-  constructor() {}
+  constructor(public readonly modal: ModalService) {
+    console.log(this.modal.visible);
+  }
 
   ngOnInit(): void {}
 }
