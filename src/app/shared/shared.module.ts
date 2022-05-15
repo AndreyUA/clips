68bf489dbe4753b21a39e78435959336ba43300a
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-15 17:11:10 +0300

    created input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Components
-import { ModalComponent } from './modal/modal.component';
-import { TabsContainerComponent } from './tabs-container/tabs-container.component';
-import { TabComponent } from './tab/tab.component';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
+import { TabsContainerComponent } from 'src/app/shared/tabs-container/tabs-container.component';
+import { TabComponent } from 'src/app/shared/tab/tab.component';
+import { InputComponent } from 'src/app/shared/input/input.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -9,13 +9,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   constructor() {}
 
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('');
   registerForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
   });
 
   ngOnInit(): void {}
